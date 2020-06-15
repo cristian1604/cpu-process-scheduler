c9583166f69b97bbe14ca4f7afe642f4ac4630a9
Cristian Bottazzi <cristian1604@educ.ar>
2020-06-15 14:18:10 -0300

    Changes added to apropiative SRT startegy

--- SRT.cpp
+++ SRT.cpp
@@ -31,75 +31,43 @@ void SRT::SolveGantt(vector <Process> &P, float &avg_wt, float &avg_st) {
 				P[j] = aux;
 			}
 		}
+		
+		// Establecer los vectores de Gantt de cada proceso en cero
+		P[i].Gantt.resize(total_burst);
+		for (int j = 0; j < total_burst; j++) {
+			P[i].Gantt[j] = NONE;
+		}
 	}
 	
 	avg_st = 0;
 	avg_wt = 0;
-	int prev_process_entry = -1;			// Tiempo en el que el proceso anterior entra al CPU
-	int prev_process_end = -1;				// Tiempo en el que el proceso anterior finaliza su ejecución en CPU
 	int remaining_bursts;					// Ráfagas de CPU restantes
 	int bt = 0;
-	vector <Process> done;
 	vector <Process> stack = P;
 	
-	while (stack.size() > 0) {
-		if (stack.size() == 0) return;
-		
+	//return; 
+	while (bt < total_burst) {
+		if (stack.empty()) break;
 		int pos = findNextProcessToExecute(P, stack, bt);
-		remaining_bursts = P[pos].getCpuTime();
+		int i = getIndex(stack, P[pos]);
 		
-		// Establecer el vector vacío
-		P[pos].Gantt.resize(total_burst);
-		for (bt = 0; bt < total_burst; bt++) {
-			P[pos].Gantt[bt] = NONE;
-		}
-		
-		// Establecer los tiempos de espera (waiting time -wt-)
-		int wt = 0;
-		for (bt = P[pos].getArrivalTime(); bt < prev_process_end; bt++) {
-			P[pos].Gantt[bt] = WAITING;
-			++wt;
-		}
-		P[pos].setWaitTime(wt);
-		avg_wt += wt;
-		P[pos].setServiceTime(wt+P[pos].getCpuTime());
-		
-		// Establecer los tiempos de ejecución
-		while (remaining_bursts > 0 && bt < total_burst) {
+		remaining_bursts = stack[i].getCpuTime();
+		while (remaining_bursts > 0) {
 			P[pos].Gantt[bt++] = PROCESSING;
-			--remaining_bursts;
-		}
-		
-		// finalizado el completado del vector, actualizo los valores del último proceso
-		prev_process_entry = P[pos].getArrivalTime();
-		prev_process_end = bt;
-		
-		done.push_back(stack[0]);
-		stack = processQueue(stack, done);
-	}
-	avg_wt /= P.size();
-}
-
-
-// La siguiente funcion devuelve un vector de elementos que restan por ser procesados
-// (cola de procesos que se encuentran en estado de "listo" y que aún no han llegado
-vector <Process> SRT::processQueue(vector <Process> all, vector <Process> executed) {
-	bool add;
-	vector <Process> res;
-	for (int i = 0; i < all.size(); i++) {
-		add = true;
-		for (int j = 0; j < executed.size(); j++) {
-			if (all[i].getId() == executed[j].getId()) {
-				add = false;
+			stack[i].setCpuTime(--remaining_bursts);
+			
+			// Buscar si no entra un proceso con un CPU burst más corto para continuar
+			int x = findNextProcessToExecute(P, stack, bt);
+			if (P[pos].getId() != stack[i].getId()) {
 				break;
 			}
 		}
-		if (add) {
-			res.push_back(all[i]);
-		}
+		
+		stack = updateStack(stack);
 	}
-	return res;
+	avg_wt = calculateWaitingTimes(P);
 }
+
 
 // Esta funcion devuelve el indice del vector principal P para el elemento elem dado
 int SRT::getIndex(vector <Process> &P, Process elem) {
@@ -111,23 +79,59 @@ int SRT::getIndex(vector <Process> &P, Process elem) {
 	return -1;
 }
 
+// Retorna solo los elementos que restan ser procesados (con CpuTime > 0)
+vector <Process> SRT::updateStack(vector <Process> stack) {
+	vector <Process> res;
+	for (int i = 0; i < stack.size(); i++) {
+		if (stack[i].getCpuTime() > 0) {
+			res.push_back(stack[i]);
+		}
+	}
+	return res;
+}
+
 // Función que determina el próximo proceso que deberá ser ejecutado por el CPU
-int SRT::findNextProcessToExecute(vector <Process> &P, vector <Process> &stack, int bt) {
+int SRT::findNextProcessToExecute(vector <Process> &P, vector <Process> &stack, int bt) {	
 	int pos = getIndex(P, stack[0]);
 	int min_cpu_time = P[pos].getCpuTime();
 	Process aux;
 	
 	for (int j = 0; j < stack.size(); j++) {
 		if (stack[j].getArrivalTime() > bt) {continue;}
-		if (stack[j].getCpuTime() < min_cpu_time) {
+		if (stack[j].getCpuTime() < min_cpu_time && stack[j].getCpuTime() > 0) {
 			min_cpu_time = stack[j].getCpuTime();
 			aux = stack[0];
 			stack[0] = stack[j];
 			stack[j] = aux;
 			pos = getIndex(P, stack[0]);
-			//break;
 		}
-	}
-	
+	}	
 	return pos;
+}
+
+// Calcular tiempos de espera entre turnos de procesamiento en RoundRobin
+// Calcula tanto los ServiceTime y los WaitTime, como también setea las esperas en el vector de Gantt
+float SRT::calculateWaitingTimes(vector <Process> &P) {
+	float res = 0;
+	int wt;
+	for (int i = 0; i < P.size(); i++) {
+		bool started = false;
+		int bursts = P[i].getCpuTime();
+		wt = 0;
+		for (int j = 0; j < P[i].Gantt.size(); j++) {
+			if (P[i].Gantt[j] == PROCESSING) {
+				started = true;
+				--bursts;
+			}
+			
+			if ((P[i].Gantt[j] == NONE && started && bursts > 0) || (P[i].Gantt[j] == NONE && P[i].getArrivalTime() <= j  && bursts > 0)) {
+				P[i].Gantt[j] = WAITING;			// seteo de espera en el vector de Gantt
+				++res;
+				++wt;
+			}
+		}
+		P[i].setServiceTime(wt+P[i].getCpuTime());
+		P[i].setWaitTime(wt);
+	}
+	return res/P.size();
 }
--- SRT.h
+++ SRT.h
@@ -9,9 +9,10 @@ public:
 	SRT();
 	void SolveGantt(vector <Process> &P, float &avg_wt, float &avg_st);
 private:
-	vector <Process> processQueue(vector <Process> P, vector <Process> done);
 	int findNextProcessToExecute(vector <Process> &P, vector <Process> &stack, int bt);
 	int getIndex(vector <Process> &P, Process elem);
+	vector <Process> updateStack(vector <Process> stack);
+	float calculateWaitingTimes(vector <Process> &P);
 };
 
 #endif

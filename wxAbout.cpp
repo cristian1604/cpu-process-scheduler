4b779c77b79c19d6ea3b83333a02ad27300cf495
Cristian Bottazzi <cristian1604@educ.ar>
2020-08-18 10:38:12 -0300

    Updated version

--- wxAbout.cpp
+++ wxAbout.cpp
@@ -1,7 +1,7 @@
 #include "wxAbout.h"
 
 wxAbout::wxAbout(wxWindow *parent) : About(parent) {
-	m_version->SetLabel("Version 0.9 - July 2020");
+	m_version->SetLabel("Version 1.0 - August 2020");
 	m_hyperlink1->SetFocus();
 }
 
